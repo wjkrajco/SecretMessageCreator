38f449897ccfc7d93cf5e37b62f603f5e47c8e3e
Billy Krajcovic <wjkrajco@ncsu.edu>
2023-10-24 18:14:22 -0400

    Small change to comment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:5173', // Change this to the location of your frontend
+  origin: 'http://127.0.0.1:5173',
   credentials: true
 }));
 
